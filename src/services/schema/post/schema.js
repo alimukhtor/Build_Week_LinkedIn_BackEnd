61d853267eb4e8d7e71f542b29d149beaaf57a37
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-01-19 15:16:45 +0000

    Update

--- src/services/schema/post/schema.js
+++ src/services/schema/post/schema.js
@@ -6,7 +6,7 @@ const PostSchema = new Schema({
     text: { type: String, required: true },
     username: { type: String, required: true},
     image: { type: String },
-    user: [{type:Schema.Types.ObjectId, ref:"Profile"}],
+    user: [{ type:Schema.Types.ObjectId, ref:"Profile" }],
 },
 { timestamps: true },
 )
